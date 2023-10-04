dc45c419b9e86dc0cace5ed36cc236b0ef0ec88e
a-rossetti <rossettiandrea276@gmail.com>
2023-10-04 17:29:56 +0200

    Fixed wrong window positioning

--- main.py
+++ main.py
@@ -52,9 +52,9 @@ class WindowArrangementUI(QWidget):
         self.add_button('ScreenPad Plus', 20, 236, 384, 110, self.show_screenpad_plus_options)
 
 
-    def move_and_resize_window(self, x, y, width, height, tile_vertically=False, tile_horizontally=False):
+    def move_and_resize_window(self, width, height, x, y, tile_vertically=False, tile_horizontally=False):
         self.run_command(f"xdotool windowactivate --sync {self.ACTIVE_WINDOW_ID} key --clearmodifiers Alt+F5")
-        self.run_command(f"xdotool windowmove {self.ACTIVE_WINDOW_ID} {x} {y} windowsize {self.ACTIVE_WINDOW_ID} {width} {height}")
+        self.run_command(f"xdotool windowsize {self.ACTIVE_WINDOW_ID} {width} {height} windowmove {self.ACTIVE_WINDOW_ID} {x} {y}")
 
         if tile_vertically:
             self.run_command(f"wmctrl -i -r {self.ACTIVE_WINDOW_ID} -b add,maximized_vert")
@@ -65,21 +65,24 @@ class WindowArrangementUI(QWidget):
     def show_main_display_options(self):
         self.clear_buttons()
         self.setFixedSize(636, 382)
-        self.add_button('Full\n Screen', 20, 20, 288, 162, lambda: self.move_and_resize_window(0, 0, 3840, 2160, True, True))
-        self.add_button('Left\n half', 328, 20, 144, 162, lambda: self.move_and_resize_window(0, 0, 1920, 2160, True))
-        self.add_button('Right\n half', 472, 20, 144, 162, lambda: self.move_and_resize_window(1920, 0, 1920, 2160, True))
-        self.add_button('Top\n left', 179, 202, 144, 81, lambda: self.move_and_resize_window(0, 0, 1920, 1080))
-        self.add_button('Top\n right', 318, 202, 144, 81, lambda: self.move_and_resize_window(1920, 0, 1920, 1080))
-        self.add_button('Bottom\n left', 179, 283, 144, 81, lambda: self.move_and_resize_window(0, 1080, 1920, 1080))
-        self.add_button('Bottom\n right', 318, 283, 144, 81, lambda: self.move_and_resize_window(1920, 1080, 1920, 1080))
+        self.add_button('Full\n Screen', 20, 20, 288, 162, lambda: self.move_and_resize_window(3840, 2160, 0, 0, True, True))
+        self.add_button('Left\n half', 328, 20, 144, 162, lambda: self.move_and_resize_window(1920, 2160, 0, 0, True))
+        self.add_button('Right\n half', 472, 20, 144, 162, lambda: self.move_and_resize_window(1920, 2160, 1920, 0, True))
+        self.add_button('Two\n thirds', 20, 202, 192, 162, lambda: self.move_and_resize_window(2560, 2160, 0, 0))
+        self.add_button('One\n third', 212, 202, 96, 162, lambda: self.move_and_resize_window(1280, 2160, 2560, 0))
+        self.add_button('Top\n left', 328, 202, 144, 81, lambda: self.move_and_resize_window(1920, 1080, 0, 0))
+        self.add_button('Top\n right', 472, 202, 144, 81, lambda: self.move_and_resize_window(1920, 1080, 1920, 0))
+        self.add_button('Bottom\n left', 328, 283, 144, 81, lambda: self.move_and_resize_window(1920, 1080, 0, 1080))
+        self.add_button('Bottom\n right', 472, 283, 144, 81, lambda: self.move_and_resize_window(1920, 1080, 1920, 1080))
 
 
     def show_screenpad_plus_options(self):
         self.clear_buttons()
         self.setFixedSize(424, 280)
-        self.add_button('Full\n Screen', 20, 20, 384, 110, lambda: self.move_and_resize_window(0, 2160, 3840, 1100, True, True))
-        self.add_button('Left\n half', 20, 150, 192, 110, lambda: self.move_and_resize_window(0, 2160, 1920, 1100, True))
-        self.add_button('Right\n half', 212, 150, 192, 110, lambda: self.move_and_resize_window(1920, 2160, 1920, 1100, True))
+        self.add_button('Full\n Screen', 20, 20, 384, 110, lambda: self.move_and_resize_window(3840, 1100, 0, 2160, True, True))
+        self.add_button('Left\n half', 20, 150, 192, 110, lambda: self.move_and_resize_window(1920, 1100, 0, 2160, True))
+        self.add_button('Right\n half', 212, 150, 192, 110, lambda: self.move_and_resize_window(1920, 1100, 1920, 2160, True))
+
 
 
 if __name__ == '__main__':
